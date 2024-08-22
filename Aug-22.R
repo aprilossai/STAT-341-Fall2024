c99ffc3f344e807448dfadb1cc757350ca1a29b1
aprilossai <135135277+aprilossai@users.noreply.github.com>
2024-08-22 16:26:39 -0400

    Add files via upload

---
+++ Aug-22.R
@@ -0,0 +1,14 @@
+# Author: April Ossai, Date: 8/22/24, Purpose: test the measures of descriptive stats 
+
+#How to calculate standard deviation in R
+X <- c(1,2,3,4,5) #c: container = holds >1 number
+sd(X)
+
+# how to calculate mean in R
+mean(X)
+
+#how to calculate median in R
+median(X)
+
+# theres no function for mode
+
