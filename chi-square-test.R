7d3d70d33520db35d58bc8a11fd83de29161287c
aprilossai <135135277+aprilossai@users.noreply.github.com>
2024-11-19 16:21:09 -0500

    Add files via upload

--- chi-square-test.R
+++ chi-square-test.R
@@ -1,1 +1,12 @@
-#Author: April
+#Author: April Ossai Date: 19/11/24, Purpose: calculate chi-square analysis
+
+#Read dummy dataset
+ data_frame <- read.csv("https://goo.gl/j6lRXD")
+
+#Apply chi square function
+chisq.test(data_frame$treatment, data_frame$improvement, correct= FALSE)
+
+ Pearson's Chi-squared test
+
+data:  data_frame$treatment and data_frame$improvement
+X-squared = 5.5569, df = 1, p-value = 0.01841
